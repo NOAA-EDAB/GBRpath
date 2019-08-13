bd8a554d522ed054a307856a18dae3ac45ba55ad
Sean Lucey <Sean.Lucey@NOAA.gov>
2019-08-13 15:40:17 -0400

    Ran ecosense, 181 of 5000 valid

--- GB_sense.R
+++ GB_sense.R
@@ -22,33 +22,18 @@ source(file.path(main.dir, 'rsim_sense_master_Jun2019.r'))
 #-------------------------------------------------------------------------------
 #Load and balance model
 load(file.path(data.dir, 'GB_balanced_params.RData'))
+#load current biomass/landings
+load(file.path(data.dir, 'GB_biomass_current.RData'))
+load(file.path(data.dir, 'GB_landings_current.RData'))
+
+#Run Rpath
+GB <- rpath(GB.params, 'Georges Bank')
 
 #Test dynamic run
-# GB <- rpath(GB.params)
 # GB.scene <- rsim.scenario(GB, GB.params, years = 2014:2113)
 # GB.scene$params$NoIntegrate[2:6] <- 0
 # GB.testrun <- rsim.run(GB.scene, method = 'AB', years = 2014:2113)
 # rsim.plot(GB.testrun, GB.params$model[Type < 3, Group])
-#Assign Pedigrees
-#0 - perfect
-#1 - No clue
-#realistic 0.1 - 0.8
-#Test
-fleets <- c('DredgeScallop', 'DredgeClam', 'Gillnet', 'Longline', 'PotTrap', 
-            'OtterTrawlSm', 'OtterTrawlLg', 'Midwater', 'OtherFisheries')
-
-GB.params$pedigree[, B := 0]
-GB.params$pedigree[Group %in% fleets, B := 0]
-GB.params$pedigree[, PB := 0]
-GB.params$pedigree[, QB := 0]
-GB.params$pedigree[, Diet := 0]
-for(igear in 1:length(fleets)){
-  setnames(GB.params$pedigree, fleets[igear], 'gear')
-  GB.params$pedigree[, gear := 0]
-  setnames(GB.params$pedigree, 'gear', fleets[igear])
-}
-
-GB <- rpath(GB.params, 'Georges Bank')
 
 #Set up sense runs
 all_years <- 2014:2063
@@ -57,7 +42,7 @@ scene <- rsim.scenario(GB, GB.params, years = all_years)
 # ----- Set up ecosense generator ----- ########################################
 # load rsim_sense_master.r
 scene$params$BURN_YEARS <- 50
-NUM_RUNS <- 50
+NUM_RUNS <- 5000
 parlist <- as.list(rep(NA, NUM_RUNS))
 kept <- rep(NA, NUM_RUNS)
 
@@ -83,3 +68,19 @@ for (irun in 1:NUM_RUNS){
 KEPT <- which(kept==T)
 nkept <- length(KEPT)
 nkept
+# 181 / 5000 = 3.6%
+GB.sense <- parlist[KEPT]
+save(GB.sense, file = file.path(data.dir, 'GB_ecosense_valid.RData'))
+
+#Run scenario
+#Atlantic herring
+for(irun in 1:length(GB.sense)){
+  run.params <- 
+}
+
+
+#Cod, Haddock, and Yellowtail
+
+
+
+
