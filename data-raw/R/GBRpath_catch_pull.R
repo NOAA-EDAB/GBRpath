f9d4df0cde2d05c17aa4b73d890d706775ecad8f
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-09-13 11:04:21 -0400

    Updated through landings

--- data-raw/R/GBRpath_catch_pull.R
+++ data-raw/R/GBRpath_catch_pull.R
@@ -1,179 +1,37 @@
 #Landings/Discard pull for GBRpath
-#User parameters----------------------------------------------------------------
-if(Sys.info()['sysname']=="Windows"){
-  main.dir <- "C:/Users/Sean.Lucey/Desktop/GBRpath"
-}
+library(data.table); library(usethis); library(here); library(dbutils);
+library(comlandr)
 
-if(Sys.info()['sysname']=="Linux"){
-  main.dir  <- "/home/slucey/slucey/GBRpath"
-}
+#Connect to database
+channel <- dbutils::connect_to_database('sole', 'slucey')
 
-data.dir <- file.path(main.dir, 'data')
-gis.dir  <- file.path(main.dir, 'gis')
-
-library(RODBC)
-uid <- 'slucey'
-cat("Oracle Password: ")
-pwd <- scan(stdin(), character(), n = 1)
-
-channel <- odbcConnect('sole', uid, pwd)
-
-#Required packages--------------------------------------------------------------
-library(Survdat); library(data.table); library(rgdal); library(fitdistrplus)
-
-
-#User functions-----------------------------------------------------------------
-#Convert output to text for RODBC query
-sqltext <- function(x){
-  out <- x[1]
-  if(length(x) > 1){
-    for(i in 2:length(x)){
-      out <- paste(out, x[i], sep = "','")
-    }
-  }
-  out <- paste("'", out, "'", sep = '')
-  return(out)
-}
-
-#Landings-----------------------------------------------------------------------
-landings.qry <- paste0("select year, month, NEGEAR, mesh, nespp3, nespp4, area, spplivlb, utilcd
-                     from stockeff.MV_CF_Landings
-                     where YEAR in (2013, 2014, 2015, 2016, 2017, 2018)")
-
-landings <- as.data.table(sqlQuery(channel, landings.qry))
-
-#Need to assign unknown skates to species
-#Food = Winter Skate
-#Bait = Little Skate
-landings[NESPP3 == 365 | NESPP3 == 373 & UTILCD == 0, NESPP3 := 367]
-landings[NESPP3 == 365 | NESPP3 == 373 & UTILCD == 7, NESPP3 := 366]
-landings[, UTILCD := NULL]
-
-#Create small/large mesh gear categories
-#July 2015 - minimum mesh size = 6.5 inches
-#https://www.greateratlantic.fisheries.noaa.gov/regs/infodocs/small_mesh_exemption.pdf
-otter.trawl <- landings[NEGEAR %in% 50:59, ]
-
-#Assign large vs small based on mesh size
-otter.trawl[MESH < 6.5,  Cat := factor('Small', levels = c('Small', 'Large'))]
-otter.trawl[MESH >= 6.5, Cat := 'Large']
-
-#Proportion unknown mesh size landings by proportion of known landings
-#Get Proportions
-lg.species <- otter.trawl[Cat == 'Large', sum(SPPLIVLB), by = NESPP3]
-sm.species <- otter.trawl[Cat == 'Small', sum(SPPLIVLB), by = NESPP3]
-setnames(lg.species, 'V1', 'Lg')
-setnames(sm.species, 'V1', 'Sm')
-otter.known <- merge(lg.species, sm.species, by = 'NESPP3', all = T)
-otter.known[is.na(Lg), Lg := 0]
-otter.known[is.na(Sm), Sm := 0]
-otter.known[, Total := Lg + Sm]
-otter.known[, Prop.Lg := Lg / Total]
-otter.known[, Prop.Sm := Sm / Total]
-
-#Separate out unknown mesh
-otter.unk <- otter.trawl[is.na(Cat), ]
-otter.unk <- merge(otter.unk, otter.known[, list(NESPP3, Prop.Lg, Prop.Sm)], 
-                   by = 'NESPP3')
-#Apply proportion and form to look like full data set
-#Large
-otter.unk.lg <- copy(otter.unk)
-otter.unk.lg[, SPPLIVLB := SPPLIVLB * Prop.Lg]
-otter.unk.lg[, Cat := 'Large']
-otter.unk.lg <- otter.unk.lg[SPPLIVLB > 0, ]
-otter.unk.lg[, c('Prop.Lg', 'Prop.Sm') := NULL]
-#Small
-otter.unk.sm <- copy(otter.unk)
-otter.unk.sm[, SPPLIVLB := SPPLIVLB * Prop.Sm]
-otter.unk.sm[, Cat := 'Small']
-otter.unk.sm <- otter.unk.sm[SPPLIVLB > 0, ]
-otter.unk.sm[, c('Prop.Lg', 'Prop.Sm') := NULL]
-#Merge together
-otter.unk <- rbindlist(list(otter.unk.sm, otter.unk.lg))
-
-#Remove previous no mesh records and insert new ones
-otter.trawl <- rbindlist(list(otter.trawl[!is.na(Cat), ], otter.unk), use.names = T)
-
-#Add back into landings
-landings[, Cat := NA]
-landings <- rbindlist(list(landings[!NEGEAR %in% 50:59, ], otter.trawl), use.names = T)
-
-#Convert landings to metric tons
-landings[, SPPLIVMT := SPPLIVLB * 0.00045359237]
-landings[, SPPLIVLB := NULL]
-
-#Remove market categories of parts
-landings <- landings[!NESPP4 %in% c(119, 123, 125, 127, 812, 819, 828, 829, 1731, 2351,
-                                    2690, 2699, 3472, as.numeric(paste(348:359, 8, sep = '')), 
-                                    3868, as.numeric(paste(469:471, 4, sep = '')), 
-                                    as.numeric(paste(480:499, 8, sep ='')), 5018, 5039, 
-                                    5261, 5265), ]
-
+GB.stat.areas <- c(521, 522, 525:526, 537, 551, 552, 561, 562)
+landings <- comlandr::get_comland_data(channel, filterByArea = GB.stat.areas,
+                                       filterByYear = 1981:1985, aggGear = T, 
+                                       aggArea = T, unkVar = 'EPU',
+                                       knStrata = c('NESPP3', 'YEAR', 'HY', 'QY',
+                                                    'MONTH', 'Fleet', 'TONCL1',
+                                                    'EPU'))
 #Merge Rpath groups
-load(file.path(data.dir, 'SOE_species_list.RData'))
-#Fix known errors
-species[COMNAME == 'EEL UNCL', RPATH := 'SouthernDemersals']
-species[COMNAME == 'GRAY TRIGGERFISH', RPATH := 'SouthernDemersals']
-species[COMNAME == 'SEA BASS,NK', RPATH := 'SouthernDemersals']
-species[COMNAME == 'SILVERSIDES', RPATH := 'SmPelagics']
-species[COMNAME == 'SMOOTH PUFFER', RPATH := 'SouthernDemersals']          
-nespp.rpath <- unique(species[!is.na(NESPP3), list(NESPP3, RPATH)])
+load(here('data-raw', 'Species_codes.RData'))
 
-landings <- merge(landings, nespp.rpath, by = 'NESPP3', all.x = T)
+land <- merge(landings$comland, unique(spp[, list(NESPP3, RPATH)]), by = 'NESPP3')
 
-#Subset landings for Georges Bank
-GB.stat.areas <- c(521:526, 551, 552, 561, 562)
-GB.landings <- landings[AREA %in% GB.stat.areas, ]
+land.index <- land[, .(Landings = sum(SPPLIVMT)), by = c('RPATH', 'Fleet', 'YEAR')]
 
-#Sum landings by Rpath node and gear
-#Fleet Categories
-#Note there will be a small and large mesh otter trawl fishery
-gcat <- c('otter', 'dredge.sc', 'pot', 'longline', 'seine', 'gillnet', 'midwater',
-          'dredge.cl')
-otter     <- 50:59
-dredge.sc <- 131:132
-pot       <- c(180:190, 200:219, 300, 301)
-longline  <- c(10, 40)
-seine     <- c(70:79, 120:129, 360)
-gillnet   <- c(100:119, 500, 510, 520)
-midwater  <- c(170, 370)
-dredge.cl  <- c(382, 386, 400)
+#Divide by the area of Georges Bank
+epu <- sf::st_read(dsn = here::here('gis', 'EPU_extended.shp'))
+epu.area <- survdat::get_area(epu, 'EPU')
+land.index[, Landings := Landings / as.numeric(epu.area[STRATUM == 'GB', Area])]
 
-#Assign gear category
-for(igear in 1:length(gcat)){
-  GB.landings[NEGEAR %in% get(gcat[igear]), RGear := gcat[igear]][]
-}
+#Clean up data set
+land.index[, Units := 'mt km^-2']
+land.index[is.na(Fleet), Fleet := 'Other']
 
-#Fix otter trawls
-GB.landings[Cat == 'Small' & RGear == 'otter', RGear := 'otter.sm']
-GB.landings[Cat == 'Large' & RGear == 'otter', RGear := 'otter.lg']
+#Input landings
+land.input <- land.index[YEAR %in% 1981:1985, .(Landings = mean(Landings, na.rm = T)),
+                         by = c('RPATH', 'Fleet')]
 
-#Assign Other Fisheries
-GB.landings[is.na(RGear), RGear := 'other']
-
-#Sum by Rpath group
-GB.sums <- GB.landings[, sum(SPPLIVMT), by = c('YEAR', 'RPATH', 'RGear')]
-setnames(GB.sums, 'V1', 'SPPLIVMT')
-
-#Get three year means
-GB.mean <- GB.sums[YEAR %in% 2013:2015, mean(SPPLIVMT), by = c('RPATH', 'RGear')]
-setnames(GB.mean, 'V1', 'SPPLIVMT')
-
-GB.current <- GB.sums[YEAR %in% 2016:2018, mean(SPPLIVMT), by = c('RPATH', 'RGear')]
-setnames(GB.current, 'V1', 'SPPLIVMT')
-
-#Need to divide landings by area of stat areas not GB
-stat.areas <- readOGR(gis.dir, 'Statistical_Areas_2010')
-
-#Generate area table
-stat.areas.area <- getarea(stat.areas, 'Id')
-GB.com.area <- stat.areas.area[Id %in% c(521:526, 551, 552, 561, 562), sum(Area)]
-
-GB.landings <- GB.mean[, SPPLIVMT := SPPLIVMT / GB.com.area][]
-save(GB.landings, file = file.path(data.dir, 'GB_landings.RData'))
-
-GB.cur.land <- GB.current[, SPPLIVMT := SPPLIVMT / GB.com.area][]
-save(GB.cur.land, file = file.path(data.dir, 'GB_landings_current.RData'))
 
 #Discards-----------------------------------------------------------------------
 #Observer data base is on nova not sole
