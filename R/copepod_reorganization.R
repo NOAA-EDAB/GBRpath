6a3500fe7da11f400b59d82ee9cce25ac98e88c6
Max <maxgrezlik@gmail.com>
2024-04-18 13:56:33 -0400

    Rewrote changes to biomass and diet based on restructuring of copepod groups. Changes now impact bio and diet input files so they can reference RPath names rather than diet matrix dimensions

--- R/copepod_reorganization.R
+++ R/copepod_reorganization.R
@@ -3,7 +3,13 @@
 # the State of the Ecosystem work
 
 # Author: Max Grezlik
-# largely following work of Sarah Weisberg in MAB-Rpath
+# Ratio calculations largely following work of Sarah Weisberg in MAB-Rpath (but GB specific here)
+# Biomass adjustments and diet adjustments change input files rather than GB.params
+
+# Order of operations should be:
+# 1. Run GBRpath_biomass_pull.R and GBRpath_diet_pull.R in GBRpath>data-raw>R
+# 2. Run this script to reapportion copepod groups in alignment with SOE work
+# 3. Run GB_initial_model.R in GBRpath>R
 
 # Load packages -----------------------------------------------------------
 
@@ -12,11 +18,12 @@ library(ggplot2) #plotting
 library(rnaturalearth) #simple map 
 library(lubridate) #dates
 library(spatialEco) #more advanced GIS
+library(data.table)
 library(dplyr) 
 library(tidyverse)
 library(here)
 library(Rpath)
-# Biomass adjustments -----------------------------------------------------
+# Ratio calculations  -----------------------------------------------------
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
@@ -105,61 +112,137 @@ copes_avg$biomass<-copes_avg$abd*copes_avg$weight
 calfin<-copes_avg %>% filter(spp_long=="CALANUS_FINMARCHICUS")
 copes_ratio<- calfin$biomass/sum(copes_avg$biomass)
 
-#load GB params
-load(here("data/GB.params.rda"))
 
-#adjust starting biomasses
+#Biomass adjustments -----------------------------------------------------
+#load bio.input.rda
+load(here("data", "bio.input.rda"))
+
 #SmCopepods
-sm_new<-GB.params$model$Biomass[which(GB.params[["model"]][["Group"]] == "SmCopepods")] + GB.params$model$Biomass[which(GB.params[["model"]][["Group"]] == "LgCopepods")] * (1-copes_ratio)
-GB.params$model$Biomass[which(GB.params[["model"]][["Group"]] == "SmCopepods")] <- sm_new
+# Biomass should be current biomass + (current biomass of LgCopepods * (1-copes_ratio))
+new.SmCopes <- bio.input[RPATH == "SmCopepods", "B"] + (bio.input[RPATH == "LgCopepods", "B"] * (1 - copes_ratio))
+
 #LgCopepods
-lg_new<-GB.params$model$Biomass[which(GB.params[["model"]][["Group"]] == "LgCopepods")] * copes_ratio
-GB.params$model$Biomass[which(GB.params[["model"]][["Group"]] == "LgCopepods")] <- lg_new
+# Biomass should be current biomass of LgCopepods * copes_ratio
+new.LgCopes <- bio.input[RPATH == "LgCopepods", "B"] * copes_ratio
+
+# Replace Biomass for SmCopepods and LgCopepods with new.SmCopes and new.LgCopes
+bio.input[RPATH == "SmCopepods", "B"] <- new.SmCopes
+bio.input[RPATH == "LgCopepods", "B"] <- new.LgCopes
+
+# Save bio.input -----------------------------------------------------
+usethis::use_data(bio.input, overwrite = T)
 
 
 # Diet adjustments --------------------------------------------------------
-#get rid of NAs first
-GB.params$diet<-replace(GB.params$diet,is.na(GB.params$diet),0)
+# load diet input data
+load(here('data', 'diet.input.rda'))
 
-#Shift AmLobster[47] predation
-#Shift 0.5% from Macrobenthos[48] to LgCopepods[59]
-GB.params$diet[48,48]<-GB.params$diet[48,48]-0.005
-GB.params$diet[59,48]<-GB.params$diet[59,48]+0.005
-#Shift 0.5% from Macrobenthos[48] to SmCopepods[60]
-GB.params$diet[48,48]<-GB.params$diet[48,48]-0.005
-GB.params$diet[60,48]<-GB.params$diet[60,48]+0.005
+#Shift AmLobster predation
+#Shift 0.5% from Macrobenthos to LgCopepods
+new.macrobenthos <- diet.input[Rprey == "Macrobenthos" & Rpred == "AmLobster", "preyper"] - 0.005
+diet.input[Rprey == "Macrobenthos" & Rpred == "AmLobster", "preyper"] <- new.macrobenthos
+# create new entry Rpred = "AmLobster" and Rprey = "LgCopepods", set to 0.005
+new.LgCopes <- data.table(
+  Rpred = "AmLobster",
+  Rprey = "LgCopepods",
+  preyper = 0.005
+)
+# add new entry to diet.input
+diet.input <- rbind(diet.input, new.LgCopes)
 
-#Shift AtlMackerel[8] predation
-#Remove 5% from Macrobenthos[48]
-#Remove 15% from Micronekton[55]
-#Remove 10% from SmPelagics[11]
-#Remove 30% from LgCopepods[59]
-#Move 60% to SmCopepods[60]
-GB.params$diet[48,9]<-GB.params$diet[48,9]-0.05
-GB.params$diet[55,9]<-GB.params$diet[55,9]-0.15
-GB.params$diet[11,9]<-GB.params$diet[11,9]-0.1
-GB.params$diet[59,9]<-GB.params$diet[59,9]-0.3
-GB.params$diet[60,9]<-GB.params$diet[60,9]+0.6
+# Shift AtlHerring Predation
+# based on NWACS values
+# Shift 22% from Krill to SmCopepods
+# Shift 18% from Micronekton to SmCopepods
+new.krill <- diet.input[Rprey == "Krill" & Rpred == "AtlHerring", "preyper"] - 0.22
+new.micronekton <- diet.input[Rprey == "Micronekton" & Rpred == "AtlHerring", "preyper"] - 0.12
+old.krill <- diet.input[Rprey == "Krill" & Rpred == "AtlHerring", "preyper"]
+old.micronekton <- diet.input[Rprey == "Micronekton" & Rpred == "AtlHerring", "preyper"]
+krill.change <- old.krill - new.krill
+micronekton.change <- old.micronekton - new.micronekton
+diet.input[Rprey == "Krill" & Rpred == "AtlHerring", "preyper"] <- new.krill
+diet.input[Rprey == "Micronekton" & Rpred == "AtlHerring", "preyper"] <- new.micronekton
 
-#Shift SmCopepod[60] predation
-#Move 0.25% from SmCopepods[60] to LgCopepods[59]
-GB.params$diet[60,61]<-GB.params$diet[60,61]-0.0025
-GB.params$diet[59,61]<-GB.params$diet[59,61]+0.0025
+# create new entry Rpred = "AtlHerring" and Rprey = "SmCopepods", set to new.krill + new.micronekton
+new.SmCopes <- data.table(
+  Rpred = "AtlHerring",
+  Rprey = "SmCopepods",
+  preyper =  krill.change + micronekton.change
+)
+# name columns
+setnames(new.SmCopes, c("Rpred", "Rprey", "preyper"))
+# add new entry to diet.input
+diet.input <- rbind(diet.input, new.SmCopes)
 
+#Shift AtlMackerel predation
+#Remove 23% from Macrobenthos and move to SmCopepods
+#Remove 5% from Micronekton
+#Remove 4% from SilverHake
+#Move 32% to SmCopepods
+new.macrobenthos <- diet.input[Rprey == "Macrobenthos" & Rpred == "AtlMackerel", "preyper"] - 0.23
+new.micronekton <- diet.input[Rprey == "Micronekton" & Rpred == "AtlMackerel", "preyper"] - 0.05
+new.silverhake <- diet.input[Rprey == "SilverHake" & Rpred == "AtlMackerel", "preyper"] - 0.04
+diet.input[Rprey == "Macrobenthos" & Rpred == "AtlMackerel", "preyper"] <- new.macrobenthos
+diet.input[Rprey == "Micronekton" & Rpred == "AtlMackerel", "preyper"] <- new.micronekton
+diet.input[Rprey == "SilverHake" & Rpred == "AtlMackerel", "preyper"] <- new.silverhake
 
-#For rest of groups, add portion to SmCope consumption
-GB.params$diet[60,c(2:8,10:47,49:60,62)]<-
-  GB.params$diet[60,c(2:8,10:47,49:60,62)]+
-  GB.params$diet[59,c(2:8,10:47,49:60,62)]*(1-copes_ratio)
-GB.params$diet[59,c(2:8,10:47,49:60,62)]<-
-  GB.params$diet[59,c(2:8,10:47,49:60,62)]*copes_ratio
+# create new entry Rpred = "AtlMackerel" and Rprey = "SmCopepods", set preyper to 0.32
+new.SmCopes <- data.table(
+  Rpred = "AtlMackerel",
+  Rprey = "SmCopepods",
+  preyper = 0.32
+)
+# add new entry to diet.input
+diet.input <- rbind(diet.input, new.SmCopes)
 
-# overwrite GB.params and initial model
-check.rpath.params(GB.params)
-usethis::use_data(GB.params, overwrite = T)
+#Shift SmCopepod predation
+#Move 0.25% from SmCopepods to LgCopepods
+new.SmCopes <- diet.input[Rprey == "SmCopepods" & Rpred == "SmCopepods", "preyper"] - 0.0025
+new.LgCopes <- diet.input[Rprey == "LgCopepods" & Rpred == "SmCopepods", "preyper"] + 0.0025
+diet.input[Rprey == "SmCopepods" & Rpred == "SmCopepods", "preyper"] <- new.SmCopes
+diet.input[Rprey == "LgCopepods" & Rpred == "SmCopepods", "preyper"] <- new.LgCopes
 
-#Initial unbalanced model
+#For all other predators of SmCopepods, change diet proportional to copes_ratio
 
-GB.init <- rpath(GB.params, 'Georges Bank', 1)
+# find species that prey on both small and large copepods
+SmCopes.preds <- diet.input[Rprey == "SmCopepods", "Rpred"]
+LgCopes.preds <- diet.input[Rprey == "LgCopepods", "Rpred"]
+# find overlapping Rpred
+Copes.preds <- intersect(SmCopes.preds, LgCopes.preds)
 
-usethis::use_data(GB.init, overwrite = T)
+# Reapportion predation by copes_ratio
+# SmPelagics
+new.SmPelagics.Sm <- diet.input[Rprey == "SmCopepods" & Rpred == "SmPelagics", "preyper"] + (diet.input[Rprey == "LgCopepods" & Rpred == "SmPelagics", "preyper"] * (1 - copes_ratio))
+new.SmPelagics.Lg <- diet.input[Rprey == "LgCopepods" & Rpred == "SmPelagics", "preyper"] * copes_ratio
+diet.input[Rprey == "SmCopepods" & Rpred == "SmPelagics", "preyper"] <- new.SmPelagics.Sm
+diet.input[Rprey == "LgCopepods" & Rpred == "SmPelagics", "preyper"] <- new.SmPelagics.Lg
+#GelZooplankton
+new.GelZooplankton.Sm <- diet.input[Rprey == "SmCopepods" & Rpred == "GelZooplankton", "preyper"] + (diet.input[Rprey == "LgCopepods" & Rpred == "GelZooplankton", "preyper"] * (1 - copes_ratio))
+new.GelZooplankton.Lg <- diet.input[Rprey == "LgCopepods" & Rpred == "GelZooplankton", "preyper"] * copes_ratio
+diet.input[Rprey == "SmCopepods" & Rpred == "GelZooplankton", "preyper"] <- new.GelZooplankton.Sm
+diet.input[Rprey == "LgCopepods" & Rpred == "GelZooplankton", "preyper"] <- new.GelZooplankton.Lg
+#Krill
+new.Krill.Sm <- diet.input[Rprey == "SmCopepods" & Rpred == "Krill", "preyper"] + (diet.input[Rprey == "LgCopepods" & Rpred == "Krill", "preyper"] * (1 - copes_ratio))
+new.Krill.Lg <- diet.input[Rprey == "LgCopepods" & Rpred == "Krill", "preyper"] * copes_ratio
+diet.input[Rprey == "SmCopepods" & Rpred == "Krill", "preyper"] <- new.Krill.Sm
+diet.input[Rprey == "LgCopepods" & Rpred == "Krill", "preyper"] <- new.Krill.Lg
+#Micronekton
+new.Micronekton.Sm <- diet.input[Rprey == "SmCopepods" & Rpred == "Micronekton", "preyper"] + (diet.input[Rprey == "LgCopepods" & Rpred == "Micronekton", "preyper"] * (1 - copes_ratio))
+new.Micronekton.Lg <- diet.input[Rprey == "LgCopepods" & Rpred == "Micronekton", "preyper"] * copes_ratio
+diet.input[Rprey == "SmCopepods" & Rpred == "Micronekton", "preyper"] <- new.Micronekton.Sm
+diet.input[Rprey == "LgCopepods" & Rpred == "Micronekton", "preyper"] <- new.Micronekton.Lg
+#Macrobenthos
+new.Macrobenthos.Sm <- diet.input[Rprey == "SmCopepods" & Rpred == "Macrobenthos", "preyper"] + (diet.input[Rprey == "LgCopepods" & Rpred == "Macrobenthos", "preyper"] * (1 - copes_ratio))
+new.Macrobenthos.Lg <- diet.input[Rprey == "LgCopepods" & Rpred == "Macrobenthos", "preyper"] * copes_ratio
+diet.input[Rprey == "SmCopepods" & Rpred == "Macrobenthos", "preyper"] <- new.Macrobenthos.Sm
+diet.input[Rprey == "LgCopepods" & Rpred == "Macrobenthos", "preyper"] <- new.Macrobenthos.Lg
+#LgCopepods
+new.LgCopes.Sm <- diet.input[Rprey == "SmCopepods" & Rpred == "LgCopepods", "preyper"] + (diet.input[Rprey == "LgCopepods" & Rpred == "LgCopepods", "preyper"] * (1 - copes_ratio))
+new.LgCopes.Lg <- diet.input[Rprey == "LgCopepods" & Rpred == "LgCopepods", "preyper"] * copes_ratio
+diet.input[Rprey == "SmCopepods" & Rpred == "LgCopepods", "preyper"] <- new.LgCopes.Sm
+diet.input[Rprey == "LgCopepods" & Rpred == "LgCopepods", "preyper"] <- new.LgCopes.Lg
+
+# Save diet.input -----------------------------------------------------
+
+usethis::use_data(diet.input, overwrite = T)
+
