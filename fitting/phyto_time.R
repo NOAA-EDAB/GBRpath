5c71d4ed0e58918583906ab31ae030dece447dcf
Max Grezlik <maxgrezlik@gmail.com>
2024-08-19 09:31:33 -0400

    created time series of phytoplankton biomass

--- fitting/phyto_time.R
+++ fitting/phyto_time.R
@@ -7,7 +7,7 @@
 # Author: M.T. Grezlik
 # Contact details: mgrezlik@umassd.edu
 # following the example of S. Weisberg
-# https://github.com/SarahJWeisberg/GOM-Rpath/blob/main/fitting/Phyto_time.R
+# https://github.com/SarahJWeisberg/GOM-Rpath/blob/main/fitting/phyto_time_ecodata.R
 
 # Load necessary libraries
 library(dplyr)
@@ -16,39 +16,130 @@ library(stringr)
 library(ggplot2)
 library(zoo)
 library(here)
+library(RCurl)
 
 # input files 
-chl_csv <- read.csv(here("data/OCCCI-V6.0-L3B4-SUBAREAS_EXTRACT.CSV"))
+curlSetOpt(timeout=200)
+ppd_csv <- read.csv("https://github.com/NOAA-EDAB/ecodata/blob/master/data-raw/19980101_20221231-OCCCI_GLOBCOLOUR-PPD-STATS_ANOMS-NES_EPU_NOESTUARIES-SOE_V2023-SOE_FORMAT.csv?raw=TRUE")
+chl_csv <- read.csv("https://github.com/NOAA-EDAB/ecodata/blob/master/data-raw/19980101_20221231-OCCCI_GLOBCOLOUR-CHLOR_A-STATS_ANOMS-NES_EPU_NOESTUARIES-SOE_V2023-SOE_FORMAT.csv?raw=TRUE")
 
-#isolate years, geometric means
-chl<-chl_csv %>% dplyr::select(PERIOD, GMEAN) %>%
-  dplyr::rename(Year = PERIOD, Value = GMEAN)
+# transformation ----
 
-#remove total mean
-chl<-chl %>% filter(Year != "ANNUAL_1998_2022")
+ppd<- ppd_csv  |> 
+        dplyr::select(PERIOD, UNITS, VARIABLE, VALUE, SUBAREA)  |> 
+        dplyr::rename(Time = PERIOD, Units = UNITS, Var = VARIABLE,
+                      EPU =SUBAREA, Value = VALUE)
 
+# filter for just GB
+ppd_gb <- ppd |> dplyr::filter(EPU == "GB")
+
+# Make numeric column for year, get rid of weekly estimates
+ppd_gb$Year<- as.numeric(str_remove(ppd_gb$Time,"A_"))
+ppd_gb <- na.omit(ppd_gb)
+
+#Make sure values are numeric
+ppd_gb$Value<-as.numeric(ppd_gb$Value)
+
+#Turn Var colum into useful categories - anomaly vs. median
+ppd_gb$Var<-str_remove(ppd_gb$Var,"ANNUAL_PPD_")
+
+#Plot both median and anomaly
+# ggplot(data = ppd_gb,aes(x=Year, y=Value))+
+#   geom_line()+
+#   stat_smooth(method = "lm")+
+#   facet_wrap(~Var, scales = "free")
+
+#Plot just anomalies
+# ppd_gb %>% filter(Var == "RATIO_ANOMALY") %>%
+#   ggplot(aes(x=Year, y=Value))+
+#   geom_point()
+
+
+chl <- chl_csv  |> 
+  dplyr::select(PERIOD, UNITS, VARIABLE, VALUE, SUBAREA)  |> 
+  dplyr::rename(Time = PERIOD, Units = UNITS, Var = VARIABLE,
+                EPU = SUBAREA, Value = VALUE)
+
+#chl_pp <- rbind(ppd,chl)%>%
+# tibble::as_tibble() %>%
+#  dplyr::select(Time, Var, Value, EPU, Units)
+
+#Filter for just GoM
+chl_gb<-chl |> 
+          filter(EPU == "GB")
 
 #Make numeric column for year, get rid of weekly estimates
-chl$Year<-as.numeric(str_remove(chl$Year,"A_"))
+chl_gb$Year<-as.numeric(str_remove(chl_gb$Time,"A_"))
+chl_gb<-na.omit(chl_gb)
 
-#Plot
-ggplot(data = chl,aes(x=Year, y=Value))+
-  geom_line()
+#Make sure values are numeric
+chl_gb$Value<-as.numeric(chl_gb$Value)
+
+#Turn Var colum into useful categories - anomaly vs. median
+chl_gb$Var<-str_remove(chl_gb$Var,"ANNUAL_CHLOR_A_")
+
+#Plot both median and anomaly
+ggplot(data = chl_gb,aes(x=Year, y=Value))+
+  geom_line()+
+  facet_wrap(~Var, scales = "free")
+
+#Plot just anomalies
+# chl_gb %>% filter(Var == "RATIO_ANOMALY") %>%
+#   ggplot(aes(x=Year, y=Value))+
+#   geom_point()
+
+# lm<-lm(Value~Year, data = chl_gb, subset = (Var == "RATIO_ANOMALY"))
+# summary(lm)
 
 #set baselines
 #prior to 2001, which is years included in EMAX estimates
 #start with ppd
-chl_EMAX<-chl %>% filter(Year < 2001) %>% summarise(mean=mean(Value))
+ppd_EMAX<-ppd_gb |> 
+  group_by(Var) |> 
+  filter(Year <2001) |> 
+  summarise(mean=mean(Value))
 
-chl$EMAX_mean<-rep(chl_EMAX$mean, length(chl$Year))
+ppd_gb <- left_join(ppd_gb,ppd_EMAX, by="Var")
 
-chl<-chl %>% mutate (anom = (Value-EMAX_mean)/EMAX_mean)
+ppd_gb$anom<-(ppd_gb$Value-ppd_gb$mean)/ppd_gb$mean
+
+#plot just after 2000
+# ppd_gb  |> 
+#   filter(Year >2000, Var == "MEDIAN") |> 
+# ggplot(aes(x=Year, y=anom))+
+#   geom_line()+
+#   #geom_line(aes(y=zoo::rollmean(anom,3,na.pad = T)))+
+#   #facet_wrap(~Var)+
+#   labs(y="Production Anomaly (gC/m^2/day)")+
+#   stat_smooth(method = "lm",col="red")
+
+#repeat with chl
+chl_EMAX<-chl_gb |> 
+  group_by(Var) |> 
+  filter(Year <2001)  |> 
+  summarise(mean=mean(Value))
+
+chl_gb <- left_join(chl_gb, chl_EMAX, by="Var")
+#chl_gb$anom <- (chl_gb$Value-chl_gb$mean)/chl_gb$mean
 
 #rescale to starting biomass value
-pp_start<-22.126
-pp_force<-chl %>% 
-  mutate(force_b = Value/EMAX_mean * pp_start) %>% dplyr::select(Year,force_b)
+pp_start<-1.977300e+01
+pp_force<-chl_gb  |> 
+  filter(Year>=2001,Var == "MEDIAN")  |>  
+  mutate(force_b = Value/mean * pp_start)  |> 
+  dplyr::select(Year,force_b)
 
-#plot
-ggplot(data = pp_force,aes(x=Year, y=force_b))+
-  geom_line()
+#add previous years
+# pre<-cbind(seq(1985,2000),rep(pp_start,16))
+# colnames(pre)<-c("Year","force_b")
+# pp_force<-rbind(pre,pp_force)
+
+#plot just after 2003
+# chl_gb |> 
+#   filter(Year >2000, Var == "MEDIAN") |> 
+#   ggplot(aes(x=Year, y=anom))+
+#   geom_line()+
+#   #geom_line(aes(y=zoo::rollmean(anom,3,na.pad = T)))+
+#  #facet_wrap(~Var, scales = "free")+
+#   labs(y="ChlA Anomaly (mg/m^3)")+
+#   stat_smooth(method = "lm",col="red")
